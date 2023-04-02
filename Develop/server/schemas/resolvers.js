b9cb492ba35a8cb265e87703f67b210448cb5f6d
Shane <shanepopowski@yahoo.com>
2023-04-01 21:40:51 -0500

    pushing to heroku

--- Develop/server/schemas/resolvers.js
+++
@@ -1,8 +0,0 @@
-const { AuthenticationError } = require ('apollo-server-express');
-const { User } = require('../models');
-const ( signToken ) = require('../utils/auth');
-const resolvers = { Query: { me: async (parent, args, context) => {
-    if (context.user) { data = await User.findOne({ _id: context.use._id}).select('-__v -password');
-    return data; }
-    throw new AuthenticationError('log in boyy');
-},},}
--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({ uri: '/graphql',});
 const authLink = setContext((_, { headers }) => { const token = localStorage.getItem('id_token');
-                                                  return { headers: { ...headers, authorization: token ? 'useing ${ token }' : '',},};});
+                                                  return { headers: { ...headers, authorization: token ? 'Bearer ${ token }' : '',},};});
 const client = new ApolloClient({ link: authLink.concat(httpLink),
                                   cache: new inMemoryCache(),});
 
