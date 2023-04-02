8d1dfd02c252446f2c3d12304927eaa9a9806f1a
Shane <shanepopowski@yahoo.com>
2023-04-01 23:30:27 -0500

    fixed bugg21

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_ENDPOINT || 'mongodb://localhost/googlebooksDb', {
+mongoose.connect(process.env.MONGODB_URI| 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
-
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
